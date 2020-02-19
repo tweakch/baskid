d97317b653a6d04405e89d44da7ddfa857da94d9
Alexander Klee <aklee@pdc-online.com>
2020-02-19 12:03:54 +0100

    remove custom command

--- src/Baskid/Program.cs
+++ src/Baskid/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
-using System.Threading.Tasks;
 using Baskid.Core;
 using Baskid.Core.Models;
 using Microsoft.EntityFrameworkCore;
@@ -22,34 +18,9 @@ namespace Baskid
             var serviceProvider = RegisterServices(args);
             var baskid = serviceProvider.GetService<CommandLineApplication>();
 
-            /*
-                Shows how to add custom Command
-
-                baskid.Command("get", command =>
-                {
-                    var urlArgument = command.Argument("[url]", "the url to get data from");
-                    command.OnExecute(async () =>
-                    {
-                        var repositories = await ProcessRepositories(urlArgument.Value);
-                        foreach (var repo in repositories)
-                        {
-                            Console.WriteLine(repo.Name);
-                            Console.WriteLine(repo.Description);
-                            Console.WriteLine(repo.GitHubHomeUrl);
-                            Console.WriteLine(repo.Homepage);
-                            Console.WriteLine(repo.Watchers);
-                            Console.WriteLine(repo.LastPush);
-                            Console.WriteLine();
-                        }
-                        return 0;
-                    });
-                });
-            */
-
             ILogger logger = serviceProvider.GetService<ILogger<Program>>();
             try
             {
-                logger.LogTrace(args.ToJson());
                 baskid.Execute(args);
             }
             catch (Exception e)
@@ -64,25 +35,18 @@ namespace Baskid
 
         private static ServiceProvider RegisterServices(string[] args)
         {
-            return new ServiceCollection()
-                    .AddLogging(cfg => {
-                        cfg.AddConsole();
-                        cfg.SetMinimumLevel(LogLevel.Trace);
-                    })
-                    .AddDbContext<BaskidContext>(options => options.UseSqlite("DataSource=:memory:"))
-                    .AddBaskid()
-.BuildServiceProvider();
-        }
-
-        private static async Task<List<Repository>> ProcessRepositories(string url = "https://api.github.com/orgs/dotnet/repos")
-        {
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-
-            var streamTask = client.GetStreamAsync(url);
-            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
-            return repositories;
+            var services = new ServiceCollection();
+            return services.AddLogging(cfg =>
+                           {
+                               cfg.AddConsole();
+                               cfg.SetMinimumLevel(LogLevel.Trace);
+                           })
+                           .AddDbContext<BaskidContext>(options =>
+                           {
+                               options.UseSqlite("DataSource=:memory:");
+                           })
+                           .AddBaskid()
+                           .BuildServiceProvider();
         }
     }
 }
